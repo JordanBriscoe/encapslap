a4ba1ca21d6f26e4bab45329cfd3d5a197d743ca
JordanBriscoe <briscoe41@gmail.com>
2019-06-18 13:44:31 -0600

    initial encapslap

---
+++ app/models/target.js
@@ -0,0 +1,11 @@
+export default class Target{
+    constructor(){
+        this.name = "Target",
+        this.health = 100,
+        this.attacks = {
+            slap: 5,
+            kick: 10,
+            punch: 15
+        }
+    }
+}
